f8cdd2606eb7329dc1cf06834274da706e59d508
Wesley Gomersall <gomersallw@gmail.com>
2025-02-17 18:53:45 -0800

    generating list for draw function

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -32,12 +32,14 @@ IUPAC_REGEX = {'A': '[Aa]', 'a': '[Aa]',
                'V': '[AaCcGg]', 'v': '[AaCcGg]',
                'N': '[AaCcTtUuGg]', 'n': '[AaCcTtUuGg]'}
 
-class FastaRecord: # WIP: need to figure out exontart and exonend
+class FastaRecord:
     '''Input header line (newline stripped) and entire sequence from each fasta entry.
 
     Attributes: 
         header (str):       Fasta header
-        sequence (str):     Fasta sequence (intron lower-case, exon upper-case) 
+        sequence (str):     Fasta sequence 
+                            (intron lower-case, exon upper-case,
+                            exactly 0 or 1 exons with length > 1)
         exonstart (int):    1-based index of first base in exon
         exonend (int):      1-based index of last base in exon
         length (int):       Length of fasta sequence
@@ -48,8 +50,18 @@ class FastaRecord: # WIP: need to figure out exontart and exonend
     def __init__(self, head, seq):
         self.header: str = head # remove (reverse-complement) here and add as an attribute? 
         self.sequence: str = seq
-        self.exonstart: int = 0 
-        self.exonend: int = 0 
+
+        exonlocation: list = [dex for dex, char in enumerate(self.sequence) if char.isupper()]
+        
+        if exonlocation != []: # check for one and only one exon in self.sequence with len > 1
+            for i in range(len(exonlocation) - 1): 
+                assert exonlocation[i] + 1 == exonlocation[i+1] # single continuous exon
+            self.exonstart: int = exonlocation[0] + 1 
+            self.exonend: int = exonlocation[-1] + 1
+        else: 
+            self.exonstart: int = None
+            self.exonend: int = None
+
         self.length: int = len(seq) 
 
     def show(self): 
@@ -65,9 +77,11 @@ class Motif:
         length (int):       Length of motif sequence
 
     Method(s): 
-        convert_regex():    Creates regex from name based on IUPAC codes.
-        locate():           Returns a list of 1-based indices of the first character of 
-                            self.regex regular expression in a given sequence.
+        convert_regex():    Creates regex from name based on 
+                            IUPAC codes.
+        locate():           Returns a list of 1-based indices of
+                            the first character of self.regex regular 
+                            expression in a given sequence.
     '''
     def __init__(self, given_motif): 
         self.name: str = given_motif.strip()
@@ -97,7 +111,7 @@ def generate_motif_list(filepath: str) -> list:
             motifs.append(Motif(mot))
     return motifs
 
-def parse_fasta(filepath: str, list_of_motifs: list) -> list: # WIP: when find_motifs() is functional, return results in a list
+def parse_fasta(filepath: str, list_of_motifs: list) -> list: 
     '''Loop through fasta file, for each record call function find_motifs()
     Return a list of lookup tables from find_motifs(). 
     Each element of the list corresponds to a fasta entry.'''
@@ -111,8 +125,8 @@ def parse_fasta(filepath: str, list_of_motifs: list) -> list: # WIP: when find_m
 
                 if not first: # create FastaRecord and generate lookup table
                     entry = FastaRecord(header, sequence)
-                    find_motifs(entry, list_of_motifs)
-                    # coordinates.append(result from find_motifs)
+                    temp_dict: dict = find_motifs(entry, list_of_motifs)
+                    coordinates.append(temp_dict)
 
                 header = line.strip()
                 sequence = "" 
@@ -122,12 +136,12 @@ def parse_fasta(filepath: str, list_of_motifs: list) -> list: # WIP: when find_m
 
         else: # create FastaRecord and generate lookup table
             entry = FastaRecord(header, sequence)
-            find_motifs(entry, list_of_motifs)
-            # coordinates.append(result from find_motifs)
+            temp_dict: dict = find_motifs(entry, list_of_motifs)
+            coordinates.append(temp_dict)
 
-    # return coordinates
+    return coordinates
 
-def find_motifs(record: FastaRecord, motifs: list): # WIP 
+def find_motifs(record: FastaRecord, motifs: list):
     '''Do something with record and return something else
     Each lookup table in this list is of the form:
         {header: [exonstart, exonend, length],
@@ -137,19 +151,18 @@ def find_motifs(record: FastaRecord, motifs: list): # WIP
          motifM: [occurance1, occurance2, ..., occuranceN]} 
     these lists do not necessarily have the same N'''
 
-    # coord_dict: dict = dict()
-    
-    record.show()
-    pass
+    coord_dict: dict = dict()
 
-    # return coord_dict
-
+    coord_dict[record.header] = [record.exonstart, record.exonend, record.length]
+    for motif in motifs: 
+        coord_dict[motif.name] = motif.locate(record) 
+    return coord_dict
 
 def get_colors(num_motifs: int) -> list: # WIP
     '''Generate list of color hex codes based on how many motifs need to be plotted.'''
     pass
 
-def draw(coordinates: list, colors: list): # WIP
+def draw(filename: str, coordinates: list, colors: list): # WIP
     '''Iterate through list of dictionaries. For each, normalize coordinates and cairo draws '''
 
     # first, determine longest fasta record (header: [2] has this information) 
@@ -166,30 +179,25 @@ def draw(coordinates: list, colors: list): # WIP
 if __name__ == "__main__":
     args = get_args()
 
-    # create list of motifs to iter through (these are of class Motif) 
     motif_list = generate_motif_list(args.motif)
     num_motifs = len(motif_list) 
 
-    # parse fasta, creating a list of lookup tables
-    # each dict in list corresponds to what needs to be drawn: 
-    #   {header: [exonstart, exonend, length],
-    #    motif1: [occurance1, occurance2, ..., occuranceN],
-    #    motif2: [occurance1, occurance2, ..., occuranceN],
-    #    ...
-    #    motifM: [occurance1, occurance2, ..., occuranceN]} # these lists do not necessarily have the same N
+    list_for_draw = parse_fasta(args.fasta, motif_list ) 
 
-    # num_records = len(<list from parse_fasta>)
-    
-    # get M colors based on number of motifs
-    # store these in a list of length M
+    num_records = len(list_for_draw)
 
-    # loop through the list of dicts, draw for each one. Normalize in separate step? 
+    colors = get_colors(num_motifs)
+
+    draw(filename, list_for_draw, colors) 
 
     ###########
     # testing #
     ###########
 
-    parse_fasta(args.fasta, motif_list ) 
+    print(num_records)
+
+    for item in list_for_draw: 
+        print(item) 
 
     for i, motif in enumerate(motif_list): 
         print(motif.name)
@@ -205,3 +213,8 @@ if __name__ == "__main__":
     print(f"length of sequence {tr.sequence}: {tr.length}")
     print(f"length of sequence {tr2.sequence}: {tr2.length}")
     print(f"length of motif {tm.name}: {tm.length}")
+
+    tr3 = FastaRecord("test header", "atcgATCGccc") 
+    print(f"{tr3.sequence}")
+    print(f"sequence length: {tr3.length}")
+    print(f"exon starts at: {tr3.exonstart}, exon ends at: {tr3.exonend}")
